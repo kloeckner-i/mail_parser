afec4c55fe364b353b14463b06e86ab5d9774675
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2022-05-17 18:06:29 +0200

    Release v0.1.0

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+# Changelog
+
+## [Unreleased]
+
+## [0.1.0] - 2022-05-17
+
+[unreleased]: https://github.com/adriankumpf/mail_parser/compare/v0.1.0...HEAD
+[0.1.0]: https://github.com/adriankumpf/mail_parser/releases/tag/v0.1.0
--- mix.exs
+++ mix.exs
@@ -1,10 +1,12 @@
 defmodule MailParser.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+
   def project do
     [
       app: :mail_parser,
-      version: "0.1.0-dev",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
