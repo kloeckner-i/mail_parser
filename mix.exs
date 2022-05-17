8e333e48822e1c2a3d2d0b71218c4868db55b691
Adrian Kumpf <8999358+adriankumpf@users.noreply.github.com>
2022-05-17 19:00:32 +0200

    Add package information

--- mix.exs
+++ mix.exs
@@ -2,6 +2,7 @@ defmodule MailParser.MixProject do
   use Mix.Project
 
   @version "0.1.0"
+  @repo_url "https://github.com/adriankumpf/mail_parser"
 
   def project do
     [
@@ -9,7 +10,10 @@ defmodule MailParser.MixProject do
       version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      description: "NIF binding of mail_parsser using Rustler",
+      package: package()
     ]
   end
 
@@ -24,7 +28,36 @@ defmodule MailParser.MixProject do
   defp deps do
     [
       {:rustler, ">= 0.0.0", optional: true},
-      {:rustler_precompiled, "~> 0.4.1"}
+      {:rustler_precompiled, "~> 0.4.1"},
+      {:ex_doc, ">= 0.0.0", only: :dev}
+    ]
+  end
+
+  defp docs do
+    [
+      main: "MailParser",
+      extras: ["CHANGELOG.md"],
+      skip_undefined_reference_warnings_on: ["CHANGELOG.md"],
+      source_ref: "v#{@version}",
+      source_url: @repo_url
+    ]
+  end
+
+  defp package do
+    [
+      files: [
+        "lib",
+        "native",
+        "checksum-*.exs",
+        "mix.exs",
+        "README.md",
+        "CHANGELOG.md",
+        "LICENSE-APACHE",
+        "LICENSE-MIT"
+      ],
+      maintainers: ["adrian"],
+      licenses: ["MIT", "Apache-2.0"],
+      links: %{"GitHub" => @repo_url}
     ]
   end
 end
